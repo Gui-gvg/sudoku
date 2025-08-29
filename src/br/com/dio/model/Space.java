04373bda82053cb4fc240ad0dabde1ba066f6c5b
Guilherme <57689593+Gui-gvg@users.noreply.github.com>
2025-08-28 22:47:44 -0300

    Update Space.java
    .

--- src/br/com/dio/model/Space.java
+++ src/br/com/dio/model/Space.java
@@ -1,38 +1,59 @@
 package br.com.dio.model;
 
+import java.util.Objects;
+
 public class Space {
-
-    private Integer actual;
-    private final int expected;
+    private final Integer expected;
     private final boolean fixed;
+    private Integer actual;
 
-
-    public Space(final int expected, final boolean fixed) {
+    public Space(Integer expected, boolean fixed) {
         this.expected = expected;
         this.fixed = fixed;
-        if (fixed){
-            actual = expected;
-        }
+        this.actual = fixed ? expected : null;
+    }
+
+    public Integer getExpected() {
+        return expected;
     }
 
     public Integer getActual() {
         return actual;
     }
 
-    public void setActual(final Integer actual) {
-        if (fixed) return;
-        this.actual = actual;
-    }
-
-    public void clearSpace(){
-        setActual(null);
-    }
-
-    public int getExpected() {
-        return expected;
-    }
-
     public boolean isFixed() {
         return fixed;
+    }
+
+    public void setActual(Integer actual) {
+        if (!fixed) {
+            this.actual = actual;
+        }
+    }
+
+    public void clear() {
+        if (!fixed) {
+            this.actual = null;
+        }
+    }
+
+    public boolean isValid() {
+        if (actual == null) return true;
+        return actual.equals(expected);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Space space = (Space) o;
+        return fixed == space.fixed && 
+               Objects.equals(expected, space.expected) && 
+               Objects.equals(actual, space.actual);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(expected, fixed, actual);
     }
 }
